9afc9c2b42e2896cad3eee49328b6a539be6b825
SinanovicEdis <edis.sinanovi5@gmail.com>
2023-01-07 21:55:47 +0100

    Dodan splash scr

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,18 @@ const config: CapacitorConfig = {
   appId: 'ES.OrderMe',
   appName: 'OrderMe',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500,
+      launchAutoHide: true,
+      showSpinner: true,
+      iosSpinnerStyle: "large",
+      spinnerColor: "#000000",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
